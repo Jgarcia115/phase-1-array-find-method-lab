4346b458a7a2ddcb731e847a3c6fbe6232478fb0
Jonathan <jono_95@yahoo.com>
2022-02-28 13:47:23 -0500

    commit tree effaa66ed9bcbaa364c09f153ebb85866c5e8515

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(collection) {
+    
+    let win = collection.find(object => (object.result === "W"))
+    if (win === undefined) {
+        return win
+    }
+    
+    else {
+    return win.year
+    }
+}
+
